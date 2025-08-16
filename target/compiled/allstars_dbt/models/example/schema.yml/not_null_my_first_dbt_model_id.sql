68682eabe1b2baa45c1ad23572992b75a75b8060
csarat <c.sarat@gmail.com>
2025-08-16 15:42:09 -0500

    dbt intial

---
+++ target/compiled/allstars_dbt/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "appdb"."reporting"."my_first_dbt_model"
+where id is null
+
+
---
+++ target/run/allstars_dbt/models/example/my_second_dbt_model.sql
@@ -0,0 +1,11 @@
+
+  create view "appdb"."reporting"."my_second_dbt_model__dbt_tmp"
+    
+    
+  as (
+    -- Use the `ref` function to select from other models
+
+select *
+from "appdb"."reporting"."my_first_dbt_model"
+where id = 1
+  );
